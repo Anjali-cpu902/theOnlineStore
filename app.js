faae5dca36ca8d2ea90f91e820769fe692042d37
Anjali-cpu902 <anjaliannantony1992@google.com>
2024-12-27 12:48:30 +0530

    customer management

---
+++ app.js
@@ -0,0 +1,59 @@
+const express=require("express")
+const app=express();
+const path=require("path")
+const dotenv=require("dotenv")
+const session = require("express-session")
+const passport = require("./config/passport")
+dotenv.config();
+const db=require("./config/db");
+const userRouter=require("./routes/userRouter")
+const adminRouter = require('./routes/adminRouter')
+db();
+
+
+
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(session({
+    secret:process.env.SESSION_SECRET,
+    resave:false,
+    saveUninitialized:true,
+    cookie:{
+        secure:false,
+        httpOnly:true,
+        maxAge:72*60*60*1000
+    }
+}))
+
+
+
+
+app.use(passport.initialize())
+app.use(passport.session())
+
+
+
+
+app.use((req,res,next)=>{
+    res.set('cache-control','no-store');
+    next()
+})
+
+
+
+app.set("view engine","ejs");
+app.set("views",[path.join(__dirname,'views/User'),path.join(__dirname,'views/Admin')]);
+app.use(express.static(path.join(__dirname,'public')));
+
+
+app.use("/admin",adminRouter)
+app.use("/",userRouter);
+
+
+
+app.listen(process.env.PORT,()=>{
+    console.log(`Server running on port`)
+});
+
+module.exports=app;
