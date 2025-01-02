000135125ce4e80245432faf2a0ebef91dd66091
Anjali-cpu902 <anjaliannantony1992@google.com>
2025-01-02 07:35:46 +0530

    brand management

--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -3,11 +3,15 @@ const router = express.Router();
 const adminController = require('../controllers/admin/adminController')
 const customerController = require('../controllers/admin/customerController')
 const categoryController = require('../controllers/admin/categoryController')
+const brandController = require('../controllers/admin/brandController')
 
 const {userAuth,adminAuth} = require('../middlewares/auth')
+const multer = require('multer')
+const storage = require("../helpers/multer")
+const uploads = multer({storage:storage})
 
 
-
+//user management
 router.get('/pageerror',adminController.pageerror)
 router.get('/login',adminController.loadLogin)
 router.post('/login',adminController.login)
@@ -16,6 +20,8 @@ router.get('/logout',adminController.logout)
 router.get('/users',adminAuth,customerController.customerInfo)
 router.get('/blockCustomer',adminAuth,customerController.customerBlocked)
 router.get('/unblockCustomer',adminAuth,customerController.customerunBlocked)
+
+//category management
 router.get('/category',adminAuth,categoryController.categoryInfo)
 router.post('/addCategory',adminAuth,categoryController.addCategory)
 router.post('/addCategoryOffer',adminAuth,categoryController.addCategoryOffer)
@@ -24,6 +30,9 @@ router.get('/listCategory',adminAuth,categoryController.getListCategory)
 router.get('/unlistCategory',adminAuth,categoryController.getUnlistCategory)
 router.get('/editCategory',adminAuth,categoryController.getEditCategory)
 router.post('/editCategory/:id',adminAuth,categoryController.editCategory)
+
+//brand management
+router.get('/brands',adminAuth,brandController.getBrandPage)
 
 
 module.exports = router;
